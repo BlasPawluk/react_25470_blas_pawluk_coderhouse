682b8cd5f8a341adb2ed7314ee7d6c3df235cf57
Blas Pawluk <blaspaw99@gmail.com>
2022-04-05 22:26:03 -0300

    Revisar ItemDetail e Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './Estilos.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { NavLink } from 'react-router-dom';
-export const Item = (props) => {
-  const { id, title, price, stock } = props.producto;
-  return (
-    <div className='resaltar'>
-      <h4>{title}</h4>
-      <div>${price}</div>
-      <div>Actualmente nos quedan: {stock}</div>
+export const Item = ({ producto }) => {
+  const { documentos, id } = producto;
+  console.log(documentos, id);
 
-      <NavLink to={`/item/${id}`}>
-        <button className='resaltar1'>Mas Info</button>
-      </NavLink>
-    </div>
+  return (
+    <ul>
+      <div className='divPadre'>
+        <div className='resaltar'>
+          <h4>{producto.title}</h4>
+          <div>${producto.price}</div>
+          <div>Actualmente nos quedan: {producto.stock}</div>
+          <NavLink to={`/item/${id}`}>
+            <button className='resaltar1'>Mas Info</button>
+          </NavLink>
+        </div>
+      </div>
+    </ul>
   );
 };
 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -5,33 +5,7 @@ import { ItemList } from './ItemList';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { dbFireBase } from './Firebase';
-import { getDocs, collection, where, query, getDoc } from 'firebase/firestore';
-
-/*export const productos = [
-  {
-    id: 1,
-    title: 'BMW',
-    price: 100,
-    stock: 10,
-    image: 'https://www.diariomotor.com/imagenes/2016/03/bmw-m3-e46-gtr-p.jpg',
-  },
-  {
-    id: 2,
-    title: 'Mercedes-Benz',
-    price: 200,
-    stock: 5,
-    image: 'https://zeperfs.com/photos/3861-f.jpg',
-  },
-  {
-    id: 3,
-    title: 'Audi',
-    price: 300,
-    stock: 8,
-    image:
-    'https://img.remediosdigitales.com/a6acd6/audi-r8-panther-edition-rwd-2021-15/840_560.jpg',
-  },
-];
-*/
+import { getDocs, collection, where, query } from 'firebase/firestore';
 
 export function ItemListContainer() {
   const [products, setProducts] = useState([]);
@@ -45,12 +19,12 @@ export function ItemListContainer() {
       .then((respuesta) => {
         const aux = [];
         respuesta.forEach((documentos) => {
-          const producto = {
+          const productos = {
             id: documentos.id,
             ...documentos.data(),
           };
-          console.log(producto);
-          aux.push(producto);
+          console.log(documentos, documentos.id);
+          aux.push(productos);
         });
         setProducts(aux);
       })
@@ -64,14 +38,18 @@ export function ItemListContainer() {
       );
       getDocs(q)
         .then((resp) =>
-          setProducts(resp.docs.map((p) => ({ productos: p.data(), id: p.id })))
+          setProducts(
+            resp.docs.map((p) => ({ documentos: p.data(), id: p.id }))
+          )
         )
         .catch((err) => console.log(err));
       console.log(q);
     } else {
       getDocs(collection(dbFireBase, 'productos'))
         .then((resp) =>
-          setProducts(resp.docs.map((p) => ({ productos: p.data(), id: p.id })))
+          setProducts(
+            resp.docs.map((p) => ({ documentos: p.data(), id: p.id }))
+          )
         )
         .catch((err) => console.log(err));
     }
@@ -81,7 +59,7 @@ export function ItemListContainer() {
       {!loading ? (
         <>{`${toast.info('Cargando...')}`}</>
       ) : (
-        <div className='divPadre'>{<ItemList productos={products} />}</div>
+        <div className='divPadre'>{<ItemList documentos={products} />}</div>
       )}
     </>
   );
